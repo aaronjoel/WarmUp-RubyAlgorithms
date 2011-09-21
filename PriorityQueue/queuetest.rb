2123f421702eb490cf48f0ffbc6b303239c10410
Nitish Upreti <nitishupreti@gmail.com>
2011-09-22 01:40:58 +0530

    Priority Queue based on Binary Heap

---
+++ PriorityQueue/queuetest.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+#Testing the Priority Queue
+
+require './priorityqueue.rb'
+
+def vanilatest()
+  
+  pq=PriorityQueue.new
+  #Good old menu driven Tester :)
+  
+  puts "Opertions for PriorityQueue"
+  
+  loop do
+    puts "Enter 1 to Insert: "
+    puts "Enter 2 to DeleteMin: "
+    puts "Enter 3 to PeekMin: "
+    puts "Enter 4 to show store: "
+    puts "Enter 5 to Quit: "
+    input=gets.to_i
+
+    if input==1 then
+      item=gets.to_i
+      pq.insert(item)
+    elsif input==2 then
+      puts pq.getM*in()
+    elsif input==3 then
+      puts pq.peekMin()
+    elsif input==4 then
+      pq.showstore()
+    else
+      break
+      
+    end 
+    
+  end
+  
+end
+
+vanilatest()
+
